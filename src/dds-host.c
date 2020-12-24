4252ef7077a6e3ee47aa3599404cafaeb37f7538
Eli Reed <sailedeer11@gmail.com>
2020-12-24 00:59:56 -0800

    Rolled back to baseline

--- src/dds-host.c
+++ src/dds-host.c
@@ -23,122 +23,33 @@
 */
 
 // C
-#include <stdint.h> // for uint8_t
-#include <string.h> // for memset()
-#include <stdio.h>  // for printf()
-#include <stdlib.h> // for exit()
+#include <stdint.h>   // for uint8_t
+#include <string.h>   // for memset()
+#include <stdio.h>    // for printf()
+#include <stdlib.h>   // for exit()
+#include <unistd.h>   // for getopt()
 
 // HIDAPI
 #include "hidapi.h"
 
+// project libraries
 #include "dds-host/dds-host.h"
-
-void print_response(MCPUSBPacket *res) {
-  int i;
-
-  for (i = 0; i < 64; i++) {
-    printf("buf[%d]: %#02x\n", i, res->__data__[i]);
-  }
-  printf("\n\n");
-}
+#include "dds-host/dds-util.h"
 
 int main(int argc, char **argv) {
   int res;
+  int fDebug;
+  const char * data_file_name = NULL;
   hid_device *handle;
+
+  // extract our command line options
+  
 
   res = dds_init(&handle);
 
   if (res == -1) {
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
-
-  // configure chip power-up settings
-  MCPChipSettings chip_settings = {0};
-  MCPResponse chip_res = {0};
-  chip_settings.gp0_des = GPIO_MODE_CS;
-  chip_settings.gp1_des = GPIO_MODE_CS;
-  chip_settings.gp3_des = GPIO_MODE_DF;
-  chip_settings.gp4_des = GPIO_MODE_DF;
-  chip_settings.gpio_def_dir_low = 0x00;
-  chip_settings.gpio_def_dir_high = 0x00;
-  chip_settings.gpio_def_out_low = 0x00;
-  chip_settings.gpio_def_out_high = 0x00;
-  chip_settings.chip_settings = 0x01;
-  chip_settings.chip_access_control = AC_MODE_UNPROTECTED;
-  res = dds_mcp_write_read(handle, CMD_SET_NVRAM_SETTINGS,
-                            SUB_CMD_SET_CHIP_PU,
-                            (MCPUSBPacket *) &chip_settings,
-                            (MCPUSBPacket *) &chip_res);
-
-  if (res == -1) {
-    exit(EXIT_FAILURE);
-  }
-
-  print_response((MCPUSBPacket *) &chip_res);
-
-  // configure spi transfer power-up settings
-  MCPSPITransferSettings spi_transfer_settings = {0};
-  MCPResponse spi_res = {0};
-  spi_transfer_settings.br3 = 0x00;
-  spi_transfer_settings.br2 = 0x1B;
-  spi_transfer_settings.br1 = 0xB7;
-  spi_transfer_settings.br0 = 0x00;
-  spi_transfer_settings.idle_cs_val_low = 0x01;
-  spi_transfer_settings.idle_cs_val_high = 0x00;
-  spi_transfer_settings.active_cs_val_low = 0xFC;
-  spi_transfer_settings.active_cs_val_high = 0x00;
-  spi_transfer_settings.cs_to_data_delay_low = 0x01;
-  spi_transfer_settings.cs_to_data_delay_high = 0x00;
-  spi_transfer_settings.last_data_to_cs_delay_low = 0x01;
-  spi_transfer_settings.last_data_to_cs_delay_high = 0x00;
-  spi_transfer_settings.inter_data_delay_low = 0x01;
-  spi_transfer_settings.inter_data_delay_high = 0x00;
-  spi_transfer_settings.bytes_per_transaction_low = 0x20;
-  spi_transfer_settings.bytes_per_transaction_high = 0x00;
-  spi_transfer_settings.spi_mode = 0x00;
-  res = dds_mcp_write_read(handle, CMD_SET_NVRAM_SETTINGS,
-                            SUB_CMD_SET_SPI_PU,
-                            (MCPUSBPacket *) &spi_transfer_settings,
-                            (MCPUSBPacket *) &spi_res);
-
-  if (res == -1) {
-    exit(EXIT_FAILURE);
-  }
-
-  print_response((MCPUSBPacket *) &spi_res);
-
-  // attempt to configure the DAC
-
-  // write to CONFIG3 - Addr: 0x04
-  uint8_t buf[16];
-  memset(buf, 0, 16);
-  MCPSPIDataTransferResponse spi_data_response;
-  memset(&spi_data_response, 0, sizeof(spi_data_response));
-  buf[0] = (0x1 << 2); // -> R/W: 1, N1N0: 00, Addr: 00100
-  buf[1] = (0x1 << 7) | (0x1 << 6);
-  res = dds_dac_write(handle,
-                buf, 1,
-                DAC_REG_CONFIG3,
-                (MCPUSBPacket *) &spi_data_response);
-
-  if (res == -1) {
-    fprintf(stderr, "DAC write failed.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  print_response((MCPUSBPacket *) &spi_data_response);
-
-  memset(&spi_data_response, 0, sizeof(spi_data_response));
-  res = dds_dac_read(handle, 1, DAC_REG_CONFIG3, (MCPUSBPacket *) &spi_data_response);
-
-  if (res == -1) {
-    fprintf(stderr, "DAC write failed.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  print_response((MCPUSBPacket *) &spi_data_response);
-
-  hid_close(handle);
 
   return EXIT_SUCCESS;
 }
