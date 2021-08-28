bec6734df4e964a51e170129b365ce1344d3c8c4
sailedeer <erobreed@uw.edu>
2021-08-28 16:43:39 -0700

    bit more cleanup

--- gen_dac_data.py
+++ gen_dac_data.py
@@ -18,14 +18,14 @@ def write2file(arr, path, ptsperfile):
             # write the address
             for i in range(min(ptsperfile, arr.shape[1] - chunk * ptsperfile)):
                 address = chunk * ptsperfile + i
-                flipped_addr = address #reverse_bit(address)  ##don't reverse it
-                f.write("0" + format(((flipped_addr>>15) & 3), 'x') + ",")
-                f.write(format(((flipped_addr>>7) & 0xFF), 'x') + ",")
-                f.write(format((flipped_addr << 1) & 0xFF, 'x') + ",")
+                # flipped_address = address #reverse_bit(address)  ##don't reverse it
+                f.write("0" + format(((address>>15) & 3), 'x') + ",")
+                f.write(format(((address>>7) & 0xFF), 'x') + ",")
+                f.write(format((address << 1) & 0xFF, 'x') + ",")
                 f.write(format((arr[0,address] >> 8) & 0xFF, 'x')+ "," )
                 f.write(format((arr[0,address]) & 0xFF, 'x')+ "," )
                 f.write(format((arr[1,address] >> 8) & 0xFF, 'x')+ "," )
-                f.write(format((arr[1,address]) & 0xFF, 'x')+ ",\n" )
+                f.write(format((arr[1,address]) & 0xFF, 'x')+ "\n" )
     print(str(numfiles) + " files successfully written")
 
 def reverse_bit(num):
@@ -37,26 +37,38 @@ def reverse_bit(num):
 
 # main entry point
 def main():
+    # extract command line arguments
     if len(sys.argv) == 1:
         path = expanduser('~/Desktop/DDS_data/')
         ptsperfile = 1200
     elif len(sys.argv) == 3:
-        # we'll be dumb and assume the user isn't screwing around
         path = sys.argv[1]
-        ptsperfile = int(sys.argv[2])
+        if type(path) != str:
+            print("Filepath isn't a string.")
+            exit(1)
+        try:
+            ptsperfile = int(sys.argv[2])
+        except:
+            print("Points per file must be an integer.")
+            exit(1)
+
     else:
         print('Usage: python.exe gen_dac_data.py <path> <points per file>')
         exit(1)
     
+    # make and clean our path if necessary
     if not exists(path):
         makedirs(path)
-    
     if path[-1] != '/':
         path = path + '/' 
 
+    # generate canned data
     arr1d = np.array([int(0x7FFF*(np.sin(i*2*np.pi/100.0)+1)) for i in range(24000)])
     arr2d = np.array([int(0x7FFF*(np.cos(i*2*np.pi/100.0)+1)) for i in range(24000)])
+
+    # combine into 1 array
     arr = np.stack((arr1d, arr2d), axis=0)
+
     write2file(arr, path, ptsperfile)
     exit(0)
 
