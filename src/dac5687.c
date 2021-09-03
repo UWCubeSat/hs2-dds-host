43f22af6e8bb17a919b0fd992d76b91d7556a41a
sailedeer <erobreed@uw.edu>
2021-09-03 15:39:44 -0700

    fixed to handle settings better and cleaned up includes

--- src/dac5687.c
+++ src/dac5687.c
@@ -26,69 +26,34 @@
 #include <stdlib.h>   // for atoi()
 #include <stdbool.h>  // for bool type
 #include <string.h>   // for memcpy()
-#include <unistd.h>   // for usleep()
 
 // HIDAPI
-#include "hidapi/hidapi.h"
+#include <hidapi.h>
 
 // project libraries
-#include "dds-host/mcp2210.h"
-#include "dds-host/dac5687.h"
-#include "dds-host/util/csv.h"
+#include "mcp2210.h"
+#include "dac5687.h"
+#include "csv.h"
 
-bool DAC5687_Configure(CSVFile *file, hid_device *handle) {
-  if (file == NULL) {
-    fprintf(stderr, "file can't be null\n");
-    return false;
-  }
+// verify that the address is writable
+#define CHECK_ADDR(addr) (addr != 0x08 && addr != 0x1A && addr < 0x1D)
 
+// verify that a multi-byte write doesn't intersect with a factory only address
+#define CHECK_BYTES(addr, bytes) (bytes <= (0x08 - startAddr) && bytes <= (0x1A - startAddr) && bytes <= (0x1D - startAddr))
+
+bool DAC5687_Configure(hid_device *handle, DAC5687Settings settings) {
   if (handle == NULL) {
     fprintf(stderr, "handle can't be null\n");
     return false;
   }
 
-  if (file->numCols != 2) {
-    fprintf(stderr, "the config specified may not be valid.\n");
-    return false;
-  }
+  // TODO: write all registers
+  uint8_t cfg_3_byte = (settings.config_3.sif_4_pin << 7) | (settings.config_3.dac_ser_data << 6) |
+                       (settings.config_3.half_rate << 5) | (settings.config_3.usb << 3) |
+                       (settings.config_3.counter_mode);
 
-  // first col represents address, second col represents byte
-  // navigate each row in the file, reading an address and byte
-  // from the file, and writing it to the DAC
-  // this function consumes ALL passed tokens and will do it's best to 
-  // write to the address, assuming the address and byte are valid
-  unsigned long long row;
-  for (row = 1; row <= file->numRows; row++) {
-    // read the address and store it as an int
-    const char * addrStr = CSV_ReadElement(file, row, 1);
-    const char * dataStr = CSV_ReadElement(file, row, 2);
-
-    // naively check for problems
-    if ((addrStr == NULL) || (strlen(addrStr) != 2)) {
-      fprintf(stderr, "specified address is invalid at Row: %lld\n", row);
-      return false;
-    }
-
-    if ((dataStr == NULL) || (strlen(dataStr) != 2)) {
-      fprintf(stderr, "specified data is invalid at Row: %lld\n", row);
-      return false;
-    }
-
-    // convert to integers
-    int addr = (int)strtol(addrStr, NULL, 16);
-    int data = (int)strtol(dataStr, NULL, 16);
-
-    // free the elements we read from the file
-    free((void *)addrStr);
-    free((void *)dataStr);
-
-    // attempt to write to the register
-    if (!DAC5687_WriteRegister(handle, (uint8_t)(addr & 0xFF), (uint8_t)(data & 0xFF))) {
-      fprintf(stderr, "Configure()->WriteRegister() failed\n");
-      return false;
-    }
-  }
-  return true;
+  
+  return DAC5687_WriteRegister(handle, CONFIG_3, cfg_3_byte);
 }
 
 bool DAC5687_WriteRegister(hid_device *handle, DAC5687Address addr, unsigned char txByte) {
@@ -97,7 +62,7 @@ bool DAC5687_WriteRegister(hid_device *handle, DAC5687Address addr, unsigned cha
     return false;
   }
 
-  if (addr == 0x08 || addr == 0x1A || addr >= 0x1D) {
+  if (!CHECK_ADDR(addr)) {
     fprintf(stderr, "can't write to address %x# as it's for factory use only\n", addr);
     return false;
   }
@@ -210,17 +175,21 @@ bool DAC5687_WriteRegisters(hid_device *handle, DAC5687Address startAddr, unsign
   unsigned int writeBytes = bytes - 1;
   unsigned char instrByte = (startAddr & 0x1F) | ((writeBytes & 0x03) << 5);
 
-  unsigned char spiTxBytes[bytes + 1];
-  unsigned char rxBuf[bytes + 1];
+  unsigned char *spiTxBytes = malloc(sizeof(unsigned char) * (bytes +1));
+  unsigned char *rxBuf = malloc(sizeof(unsigned char) * (bytes +1));
 
   spiTxBytes[0] = instrByte;
-  memcpy(&spiTxBytes[1], txBytes, bytes);
-  memset(rxBuf, 0, sizeof(rxBuf));
+  memcpy((spiTxBytes + 1), txBytes, bytes);
+  memset(rxBuf, 0, bytes + 1);
 
   if (MCP2210_SpiDataTransfer(handle, sizeof(spiTxBytes), spiTxBytes, rxBuf, &spiSettings) < 0) {
     fprintf(stderr, "WriteRegisters() failed\n");
+    free(spiTxBytes);
+    free(rxBuf);
     return false;
-  } 
+  }
+  free(spiTxBytes);
+  free(rxBuf); 
   return true;
 }
 
