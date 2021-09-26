71cbfef88cee404db4964b171dceb75b8253085a
Eli Reed <sailedeer11@gmail.com>
2021-09-26 13:55:07 -0700

    started implementing some functions, needs work

--- src/cfg.c
+++ src/cfg.c
@@ -7,32 +7,52 @@
 #include <fcntl.h>      // for POSIX
 #include <sys/stat.h>   // for fstat
 #include <errno.h>      // for errno
+#include <string.h>     // for string things
 
 // CFG
 #include "cfg.h"
 
 // parses a single element out of a string
-static ConfigElement * CFG_ParseElement(char *str) {
-    return true;
+static bool CFG_ParseElement(char *str, ConfigElement *out) {
+  return true;
 }
 
 // parses a config block
-static ConfigBlock * CFG_ParseBlock(char *str) {
-    return true;
+static bool CFG_ParseBlock(char *str, ConfigBlock *out) {
+  if (out == NULL || str == NULL) {
+    return false;
+  }
+
+  // extract the block name
+  const char *start = str;
+  const char *tok = strtok(str, "\"");
+  size_t size = tok - start;
+  if (size > MAX_LENGTH - 1) {
+    return false;
+  }
+
+  
+
+  memcpy(out->key, start, size);
+  out->key[size] = '\0';
+
+  // start going line-by-line and getting the keys and values
+  for (tok = strtok(NULL, "\n"); tok && *tok && !(*tok == CLOSE_BRACK); tok = strtok(NULL, "\n")) {
+    start = tok;
+
+  }
+  
+  return true;
 }
 
-// parses a buffer containing some unknown number of blocks
-static bool CFG_ParseBuffer(unsigned char *buf, Config *out) {
-    if (buf == NULL) {
-        return false;
-    }
-
+static int CFG_GetBlockCount(char *str) {
     // first, we need to do a pass through the buffer to find 
     // the number of blocks
     int num_blocks = 0;
     bool first_found = false;
-    for (int i = 0; i < strlen(buf); i++) {
-        char c = buf[i];
+    size_t buf_len = strlen(str);
+    for (int i = 0; i < buf_len; i++) {
+        char c = str[i];
         // Some possibilities
             // 1: neither found and next character is opening - start
             // 2: neither found and next character is close - error
@@ -51,30 +71,71 @@ static bool CFG_ParseBuffer(unsigned char *buf, Config *out) {
                     !first_found && c == CLOSE_BRACK) {
             // error
             fprintf(stderr, "Malformed config file. Please ensure that all blocks are enclosed in a {...} pair.\n");
-            return false;
+            return -1;
         }
+        // else we simply continue
     }
-    // now we know how many blocks to allocate
-    ConfigBlock *blocks = (ConfigBlock *) malloc(sizeof(ConfigBlock) * num_blocks);
+    return num_blocks;
+}
 
-    // second pass, we want to remove all comment strings from the file
-    // comments are delimited by an opening ';' and a terminal '\n'
-    bool comment_char_found = true;
-    for (int i = 0; i < strlen(buf); i++) {
-        char c = buf[i];
+// parses a buffer containing some unknown number of blocks
+static bool CFG_ParseBuffer(unsigned char *buf, Config *out) {
+    int num_blocks;
+    ConfigBlock *blocks;
 
-        if (!comment_char_found && c == COMMENT_CHAR) {
-            comment_char_found = true;
-        } else if (comment_char_found && c == '\n') {
-            comment_char_found = false;
-        } else {
-            buf[i] = '\0';
-        }
+    if (buf == NULL) {
+        return false;
     }
 
+    if ((num_blocks = CFG_GetBlockCount(buf)) < 0) {
+      return false;
+    }
     out->num_blocks = num_blocks;
-    out->blocks = blocks;
     return true;
+    // blocks = (ConfigBlock *) malloc(sizeof(ConfigBlock) * num_blocks);
+
+    // steps:
+      // 1) navigate to start of block from current position in buffer
+      // 2) consume characters until we encounter either an opening bracket or a space (short-circuit on space)
+      // 3) start consuming newlines, each time checking if the next character is a comment character
+      //    3.5) if it is a comment character, move on to the next new-line
+      // 4) in the absence of a comment character, consume characters until we're not at a space
+      // 5) save pointer to beginning of this part of string
+      // 6) consume characters until we encounter the element delimiter
+      // 7) copy most recent string into current block key
+      // 8) save pointer to beginning of value
+      // 9) navigate until we encounter newline or comment character
+      // 10) compute value string and store it in current config block value
+    
+    // TODO: figure out how to handle comments
+    // const char *tok;
+    // unsigned int current_block = 0;
+    // for (tok = strtok(buf, "\""); tok && *tok; tok = strtok(NULL, "\"")) {
+    //   // extract the config block string from the buffer
+    //   const char * start = tok;
+    //   tok = strtok(NULL, "}");
+    //   if (tok == NULL) {
+    //     fprintf(stderr, "Malformed config file detected. Please check formatting.\n");
+    //     free(blocks);
+    //     return false;
+    //   }
+    //   size_t size = tok - start;
+    //   char block_str[size + 1];
+    //   memcpy(block_str, start, size);
+    //   block_str[size + 1] = '\0';
+
+    //   if (!CFG_ParseBlock(block_str, (blocks + current_block))) {
+    //     fprintf(stderr, "Failed to parse config block.\n");
+    //     free(blocks);
+    //     return false;
+    //   }
+    //   current_block++;
+    // }
+    
+
+    // out->num_blocks = num_blocks;
+    // out->blocks = blocks;
+    // return true;
 }
 
 bool CFG_ParseConfigFile(int fd, Config *out) {
@@ -97,7 +158,7 @@ bool CFG_ParseConfigFile(int fd, Config *out) {
 
     int bytes_read = 0;
     // allocate sufficient buffer with space for null-character
-    unsigned char *buf = (unsigned char *) malloc(info.st_size + 1);
+    char *buf = (char *) malloc(info.st_size + 1);
     memset(buf, '\0', info.st_size + 1);
 
     const size_t kReadSize = 1024;
